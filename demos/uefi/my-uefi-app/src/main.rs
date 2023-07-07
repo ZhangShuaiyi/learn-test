7eb8cc6a9e520488f61019eb4dcee692c876ab07
Shuaiyi Zhang <zhangsy28@lenovo.com>
2023-07-07 17:13:03 +0800

    uefi demo: use system_table.stdout

--- demos/uefi/my-uefi-app/src/main.rs
+++ demos/uefi/my-uefi-app/src/main.rs
@@ -1,13 +1,21 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use log::info;
 use uefi::prelude::*;
 
 #[entry]
 fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-    info!("Hello world!");
-    system_table.boot_services().stall(10_000_000);
+    info!("Hello First!");
+    system_table.boot_services().stall(5_000_000);
+    system_table.stdout().clear().unwrap();
+    info!("Hello Second!");
+    system_table.boot_services().stall(5_000_000);
+
+    system_table.stdout().write_str("Hello Third!\n").unwrap();
+    system_table.boot_services().stall(5_000_000);
     Status::SUCCESS
 }
